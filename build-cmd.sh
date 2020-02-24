936f834373cfa2137460eb0e6d89582b2ccdae2d
Rye Mutt <rye@alchemyviewer.org>
2020-02-24 05:51:33 -0500

    Update fmodstudio to 2.00.07

--- build-cmd.sh
+++ build-cmd.sh
@@ -10,8 +10,8 @@ set -e
 set -u
 
 FMOD_ROOT_NAME="fmodstudioapi"
-FMOD_VERSION="20006"
-FMOD_VERSION_PRETTY="2.00.06"
+FMOD_VERSION="20007"
+FMOD_VERSION_PRETTY="2.00.07"
 
 top="$(pwd)"
 stage="$top"/stage
