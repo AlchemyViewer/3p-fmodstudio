1062aeac722e03fde4c81418187d4a7386e1a98e
Rye Mutt <rye@alchemyviewer.org>
2021-12-10 20:41:12 -0500

    Update to 2.02.04

--- build-cmd.sh
+++ build-cmd.sh
@@ -10,8 +10,8 @@ set -e
 set -u
 
 FMOD_ROOT_NAME="fmodstudioapi"
-FMOD_VERSION="20203"
-FMOD_VERSION_PRETTY="2.02.03"
+FMOD_VERSION="20204"
+FMOD_VERSION_PRETTY="2.02.04"
 
 top="$(pwd)"
 stage="$top"/stage
