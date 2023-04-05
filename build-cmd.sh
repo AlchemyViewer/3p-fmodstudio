bbc5b42d99ce9b26018390b5ed284f623d1e4d84
Rye Mutt <rye@alchemyviewer.org>
2023-04-04 20:17:12 -0400

    fmod 2.02.13

--- build-cmd.sh
+++ build-cmd.sh
@@ -10,8 +10,8 @@ set -e
 set -u
 
 FMOD_ROOT_NAME="fmodstudioapi"
-FMOD_VERSION="20212"
-FMOD_VERSION_PRETTY="2.02.12"
+FMOD_VERSION="20213"
+FMOD_VERSION_PRETTY="2.02.13"
 
 top="$(pwd)"
 stage="$top"/stage
