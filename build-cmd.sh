01949a7859c6eee162b283a123c1164c80c4b18d
Rye Mutt <rye@alchemyviewer.org>
2023-07-18 11:26:14 -0400

    Update FMOD to 2.02.16

--- build-cmd.sh
+++ build-cmd.sh
@@ -10,8 +10,8 @@ set -e
 set -u
 
 FMOD_ROOT_NAME="fmodstudioapi"
-FMOD_VERSION="20214"
-FMOD_VERSION_PRETTY="2.02.14"
+FMOD_VERSION="20216"
+FMOD_VERSION_PRETTY="2.02.16"
 
 top="$(pwd)"
 stage="$top"/stage
