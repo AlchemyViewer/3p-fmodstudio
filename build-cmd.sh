a9820f9fd4bac99526fbab44ce2e6eb89d463012
Rye Mutt <rye@alchemyviewer.org>
2024-06-08 17:08:53 -0400

    fmodstudio 2.02.22

--- build-cmd.sh
+++ build-cmd.sh
@@ -10,8 +10,8 @@ set -e
 set -u
 
 FMOD_ROOT_NAME="fmodstudioapi"
-FMOD_VERSION="20220"
-FMOD_VERSION_PRETTY="2.02.20"
+FMOD_VERSION="20222"
+FMOD_VERSION_PRETTY="2.02.22"
 
 top="$(pwd)"
 stage="$top"/stage
